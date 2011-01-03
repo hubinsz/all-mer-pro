26e948107b29bb349c66830a8f0cf794d217f5db
hubin <hubin@localhost>
2011-01-03 18:58:14 +0800

    commit message 1

---
+++ src/sz/hb/first/mercurial/pro/HelloMoney.java
@@ -0,0 +1,13 @@
+package sz.hb.first.mercurial.pro;
+
+public class HelloMoney {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello, hubin!");
+	}
+
+}
